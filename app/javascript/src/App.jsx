4ed3b8532806135d1ff1d76430af2ce914b4e170
Ravi Jyoti <ravijyoti3@gmail.com>
2022-09-29 17:30:07 +0530

    Added missing import statements in app.jsx

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,6 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+
+import { setAuthHeaders } from "apis/axios";
+import { initializeLogger } from "common/logger";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
