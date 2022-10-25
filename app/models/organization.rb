6bf13938d0bb10f3103f61b2889d7747202f4271
ravijyoti3 <ravijyoti3@gmail.com>
2022-10-26 04:52:07 +0530

    Added test for user and organization model

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 class Organization < ApplicationRecord
-  validates :name, presence: true
+  MAX_NAME_LENGTH = 50
+  validates :name, presence: true, length: { maximum: MAX_NAME_LENGTH }
   has_secure_password validations: false
   has_secure_token :authentication_token
-  validates :password, length: { minimum: 6 }, if: -> { password.present? }
+  validates :password, length: { minimum: 6 },
+    format: {
+      with: /[^wd](([0-9]+.[A-Za-z]+.)|[A-Za-z]+.([0-9]+.*))/,
+      message: "requires 1 letter and 1 number"
+    }, if: -> { password.present? }
   has_many :users
 end
