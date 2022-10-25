34d55c5567ed2413887636ef2bce8ce4c9d36832
ravijyoti3 <ravijyoti3@gmail.com>
2022-10-25 21:28:47 +0530

    Created user model

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  MAX_NAME_LENGTH = 50
+  MAX_EMAIL_LENGTH = 255
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
+
+  has_many :articles
+  has_many :categories
+
+  validates :name, presence: true, length: { maximum: MAX_NAME_LENGTH }
+  validates :email, presence: true,
+    uniqueness: { case_sensitive: false },
+    length: { maximum: MAX_EMAIL_LENGTH },
+    format: { with: VALID_EMAIL_REGEX }
+
+  before_save :to_lowercase
+
+  private
+
+    def to_lowercase
+      email.downcase!
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_24_183138) do
+ActiveRecord::Schema.define(version: 2022_10_25_154651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,9 @@ ActiveRecord::Schema.define(version: 2022_10_24_183138) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "body", null: false
     t.integer "status", default: 0
-    t.integer "category_id"
+    t.integer "category_id", null: false
     t.string "slug"
+    t.integer "user_id", default: 1, null: false
   end
 
   create_table "categories", force: :cascade do |t|
@@ -32,6 +33,7 @@ ActiveRecord::Schema.define(version: 2022_10_24_183138) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "position", null: false
+    t.integer "user_id", default: 1, null: false
   end
 
   create_table "redirections", force: :cascade do |t|
@@ -50,5 +52,15 @@ ActiveRecord::Schema.define(version: 2022_10_24_183138) do
     t.string "authentication_token"
   end
 
-  add_foreign_key "articles", "categories"
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "email", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  add_foreign_key "articles", "categories", on_delete: :cascade
+  add_foreign_key "articles", "users"
+  add_foreign_key "categories", "users", on_delete: :cascade
 end
