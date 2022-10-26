900c1ca6b4d38b3ae52bbbce52cbd9d9739687b7
ravijyoti3 <ravijyoti3@gmail.com>
2022-10-26 06:49:44 +0530

    Added test for organization controller

--- test/controllers/sites_controller_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require "test_helper"
-
-class OrganizationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
