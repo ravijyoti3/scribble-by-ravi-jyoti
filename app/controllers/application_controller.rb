a6edc5c6acc1d15e4d327fe3b94900de89d1c94d
ravijyoti3 <ravijyoti3@gmail.com>
2022-10-26 06:00:56 +0530

    Added test for article controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include ApiResponders
+  include ApiRescuable
+
+  private
+
+    def load_current_organization!
+      @current_organization = Organization.first
+    end
+
+    def current_user!
+      current_organization = load_current_organization!
+      @_current_user ||= @current_organization.users.first
+    end
 end
