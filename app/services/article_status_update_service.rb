67df058b1b35aa46145ce5eb697b74d631e7bff3
ravijyoti3 <ravijyoti3@gmail.com>
2022-12-24 00:34:59 +0530

    Fixed code as per review

--- app/services/article_status_update_service.rb
+++ app/services/article_status_update_service.rb
@@ -3,7 +3,14 @@
 class ArticleStatusUpdateService
   def process
     Schedule.all.each do |schedule|
-      ArticleUpdationWorker.perform_async(schedule.id)
+      ArticleUpdationWorker.perform_async(schedule.id) if schedule_to_execute?(schedule)
     end
   end
+
+  private
+
+    def schedule_to_execute?(schedule)
+      (schedule.publish_at.present? && schedule.publish_at.past?) ||
+        (schedule.unpublish_at.present? && schedule.unpublish_at.past?)
+    end
 end
